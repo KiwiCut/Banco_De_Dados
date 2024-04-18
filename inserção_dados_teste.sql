02610dd988275a4394f71fceb34b6086309a9ea4
WCSOU <werisleysouza06@gmail.com>
2024-04-18 18:02:09 -0300

    Começo das inserções para teste do banco de dados

---
+++ inserção_dados_teste.sql
@@ -0,0 +1,10 @@
+-- T A B E L A   D E   C L I E N T E S
+exec kiwicut.incluirCliente 'Abner', 'Dias', 'abnerdias@gmail.com','11751384186', 'senha123','13532122030','65607170','20-11-2000'
+exec kiwicut.incluirCliente 'Abner', 'Jonas', 'abnerJonas@gmail.com','11751384189', 'senha123','13532122060','65607170','20-11-2001'
+exec kiwicut.incluirCliente 'Maria', 'Carolnia','mariacarolina@gmail.com','829462801562', 'senha123','63095487123','76813544','09-05-2005'
+
+
+
+
+
+SELECT * FROM kiwicut.Cliente
