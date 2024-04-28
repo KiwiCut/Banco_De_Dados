a477bef52d0699670e0f408cf7ee471d0de61548
WCSOU <werisleysouza06@gmail.com>
2024-04-28 20:11:16 -0300

    REFACTOR: Incluindo alguns dados de teste dos processos no banco de dados

--- inserção_dados_teste.sql
+++ inserção_dados_teste.sql
@@ -3,8 +3,28 @@ exec kiwicut.incluirCliente 'Abner', 'Dias', 'abnerdias@gmail.com','11751384186'
 exec kiwicut.incluirCliente 'Abner', 'Jonas', 'abnerJonas@gmail.com','11751384189', 'senha123','13532122060','656071701','20-11-2001'
 exec kiwicut.incluirCliente 'Maria', 'Carolnia','mariacarolina@gmail.com','829462801562', 'senha123','63095487123','76813544','09-05-2005'
 
+-- T A B E L A   D E   A R T I S T A S
+insert into kiwicut.Artista 
+values
+    ('RacionaisMC'),
+    ('Matue'),
+    ('Teto'),
+    ('Gustavo Lima')
+
+-- T A B E L A   D E   S H O W 
+insert into kiwicut.Show
+VALUES
+    (1,'Rap in Cena','934653840','29-10-2023')
+
+
+-- T A B E L A   D E   I N G R E S S O 
+insert into kiwicut.Ingresso
+VALUES
+    ('13532122030',3,5)
+
 
 
 select * from kiwicut.Cliente
-
-SELECT * FROM kiwicut.Cliente
+SELECT * from kiwicut.Ingresso
+select * from kiwicut.Show
+SELECT * FROM kiwicut.Artista
